b551a7a13c33f74243b7d20f779cd27567fc7762
PhilippAusHeidelberg <110686148+PhilippAusHeidelberg@users.noreply.github.com>
2022-12-22 14:03:39 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonEvent(Button.A, ButtonEvent.Click, function () {
     basic.showIcon(IconNames.SmallSquare)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
     basic.showString("-")
     basic.showNumber(randint(1, 6), 150)
+})
+input.onButtonEvent(Button.B, ButtonEvent.Click, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
